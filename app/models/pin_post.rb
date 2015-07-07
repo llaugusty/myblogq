0f5be773275ed032180ba6e5ac76fd3aed705ee7
manhquynhpro123 <manhquynhpro123@gmail.com>
2015-07-07 00:11:47 -0700

    Add stylesheet for show pin

--- app/models/pin_post.rb
+++ app/models/pin_post.rb
@@ -1,6 +1,6 @@
 class PinPost < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>"}
+	has_attached_file :image, :styles => { :medium => "300x300>", :large => "768x768"}
   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
